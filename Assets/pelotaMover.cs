6566d028ee9b61b21e069b54b0e54c4295d63cac
Nicoleta <meikosayuki@gmail.com>
2018-12-12 20:31:00 +0100

    Initial commit

---
+++ Assets/pelotaMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pelotaMover : MonoBehaviour {
+    float velocidad = 0.1f;
+    public KeyCode teclaAsignada;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(teclaAsignada))
+            this.transform.Translate(new Vector3(0, velocidad, 0));
+
+        //Debug.Log("he pulsado");
+    }
+}
